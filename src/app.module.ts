7c9a2e96521d43b83f1a764222acaa25931d4aec
Gabriela <gabrielagheleno@gmail.com>
2022-09-10 13:03:07 -0300

    Entrega projeto base To Do List

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+import { TarefaModule } from './tarefa/modules/tarefa.modules';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'db_todo',
+      entities: [Tarefa],
+      synchronize: true
+    }),
+    TarefaModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
